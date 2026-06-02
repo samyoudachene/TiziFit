827b97ba418220bce9e650c6042c5c442164f0cf
samyoudachene <samyoudachene@gmail.com>
2026-06-02 14:07:34 +0100

    app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Config Mail
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 465))
+app.config['MAIL_USE_TLS'] = False                           
+app.config['MAIL_USE_SSL'] = True                             
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
